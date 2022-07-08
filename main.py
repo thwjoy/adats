ad6a9bab27ee1fbac711dda9a029764737274d1b
Tom Joy <thwjoy@gmail.com>
2022-07-08 11:48:35 +0100

    adding download

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from torch.utils.data import DataLoader
 
 from calibration import TemperatureScaling
 from metrics import ECELoss
-from net import resnet18
+from net import resnet50
 
-net = resnet18(True, num_classes=10) 
+net = resnet50(1.0, True, num_classes=10) 
 
 # TODO convert net to ResNet50
 # TODO add ts val
@@ -30,11 +30,7 @@ val_loader = torch.utils.data.DataLoader(val_ds, batch_size=256,
                                         shuffle=True, num_workers=0)
 test_loader = torch.utils.data.DataLoader(test_ds, batch_size=256,
                                           shuffle=True, num_workers=0)
-
-train_set = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform=transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=256,
-                                           shuffle=True, num_workers=0)                                        
+                                      
 
 def test(model, loader):
     preds = []
@@ -50,37 +46,6 @@ def test(model, loader):
     
     return acc.item(), ece.item()
 
-# Train a simple network
-import torch.optim as optim
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.fc.parameters(), lr=0.001, momentum=0.9)
-
-for epoch in range(1):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-        break
-
-print('Finished Training')
 net.eval()
 print("#" * 100)
 print("Plain: Acc %.3f, ECE %.3f" % test(net, test_loader))
